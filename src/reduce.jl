714e855bdd44d0c2e24c1cc303132ac6ed639c13
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-22 23:49:17 -0700

    Fix/improve documentation (#39)
    * Fix/improve documentation
    
    * More explanation in at-reduce docstring

--- src/reduce.jl
+++ src/reduce.jl
@@ -8,6 +8,9 @@
 Declare how accumulators are updated in the sequential basecase and
 how the resulting accumulators from two basecases are combined.
 
+The arguments `accᵢ` and `xᵢ` must be symbols except for `xᵢ` of the
+last two forms in which an expression can be used at `xᵢ`.
+
 In the first form,
 
 ```julia
@@ -17,11 +20,43 @@ function ((acc₁, acc₂, ..., accₙ), (x₁, x₂, ..., xₙ))
 end
 ```
 
-should be an associative function.  If `initᵢ` is specified, the tuple
-`(init₁, init₂, ..., initₙ)` should be the identify of this
-associative function.
+should be an associative function.
 
 In the last two forms, every `opᵢ` should be an associative function.
+
+If `initᵢ` is specified, the tuple `(init₁, init₂, ..., initₙ)` should
+be the identify of the related associative function.  `accᵢ = initᵢ`
+is evaluated for each basecase (each `Task`) in the beginning.
+
+Consider a loop with the following form
+
+```julia
+@floop for ...
+    # code computing (x₁, x₂, ..., xₙ)
+    @reduce() do (acc₁ = init₁; x₁), ..., (accₙ = initₙ; xₙ)
+        # code updating (acc₁, acc₂, ..., accₙ) using (x₁, x₂, ..., xₙ)
+    end
+end
+```
+
+This is converted to
+
+```julia
+acc₁ = init₁
+...
+accₙ = initₙ
+for ...
+    # code computing (x₁, x₂, ..., xₙ)
+    # code updating (acc₁, acc₂, ..., accₙ) using (x₁, x₂, ..., xₙ)
+end
+```
+
+for computing `(acc₁, acc₂, ..., accₙ)` of each basecase.  The
+accumulators `accᵢ` of two basecases are combined using "code updating
+`(acc₁, acc₂, ..., accₙ)` using `(x₁, x₂, ..., xₙ)`" where `(x₁, x₂,
+..., xₙ)` are replaced with `(acc₁, acc₂, ..., accₙ)` of the next
+basecase.  Note that "code computing `(x₁, x₂, ..., xₙ)`" is not used
+for combining the basecases.
 
 # Examples
 ```julia
