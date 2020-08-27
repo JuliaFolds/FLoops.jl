32999f40b8c2ca78b29c54c329fd862a96fe1598
Takafumi Arakaki <aka.tkf@gmail.com>
2020-08-26 22:50:05 -0700

    Don't use at-eval support of at-testset_error

--- examples/reference/reduction.jl
+++ examples/reference/reduction.jl
@@ -23,11 +23,13 @@ end == 110
 
 # ### Argument symbols must be unique within a `@reduce` block
 
-@testset_error @eval try
-    @floop for x in 1:10
-        @reduce() do (a; x), (b; x)
-            a += x
-            b *= x
+@testset_error try
+    @dedent @eval begin
+        @floop for x in 1:10
+            @reduce() do (a; x), (b; x)
+                a += x
+                b *= x
+            end
         end
     end
 catch err
