9fcc7c37c8f72c5e4c29cfa8c5ae6ef87bf473a7
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-19 16:21:32 -0500

    More tests

--- test/FLoopsTests/src/test_ast_tools.jl
+++ test/FLoopsTests/src/test_ast_tools.jl
@@ -33,4 +33,21 @@ function test_is_dotcall()
     @test !is_dotcall(:(f.a(b)))
 end
 
+function test_is_dotcall_nargs()
+    @test is_dotcall(:(a .+ b), 2)
+    @test is_dotcall(:(.√a), 1)
+    @test is_dotcall(:(f.(x)), 1)
+    @test is_dotcall(:((f.a).(b)), 1)
+    @test !is_dotcall(:(a .+ b), 1)
+    @test !is_dotcall(:(.√a), 2)
+    @test !is_dotcall(:(f.(x)), 2)
+    @test !is_dotcall(:((f.a).(b)), 2)
+    @test !is_dotcall(:(a + b), 2)
+    @test !is_dotcall(:(√a), 1)
+    @test !is_dotcall(:(f(x)), 1)
+    @test !is_dotcall(:(f.a), 1)
+    @test !is_dotcall(:((f.a)(b)), 1)
+    @test !is_dotcall(:(f.a(b)), 1)
+end
+
 end  # module
